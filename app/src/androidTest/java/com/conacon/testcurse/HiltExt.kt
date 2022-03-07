d63b4b239962f38c58e237b32808d4d6db415a9a
Sxrdnx <serdna.alejandro@gmail.com>
2022-03-07 00:20:24 -0600

    testFragment_p1

---
+++ app/src/androidTest/java/com/conacon/testcurse/HiltExt.kt
@@ -0,0 +1,16 @@
+package com.conacon.testcurse
+
+import android.os.Bundle
+import androidx.annotation.StyleRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+
+inline fun <reified T: Fragment> launchFragmentInHiltContainer(
+    fragmentArgs: Bundle? = null,
+    @StyleRes themeResId: Int = R.style.Theme_AppCompat,
+    fragmentFactory: FragmentFactory? = null,
+    crossinline action: T.()-> Unit = {}
+){
+    
+
+}
